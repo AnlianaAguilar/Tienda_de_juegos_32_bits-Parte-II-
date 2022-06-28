f0bd53332a13295567c5a1f7da1e4594225b071d
anlianaaguilar <anlianaaguilar@gmail.com>
2022-06-28 05:10:46 -0400

    carpetas

---
+++ src/components/GameList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <h1 class="subtitle">Lista de Juegos</h1>
+    <div class="container_table">
+      <table>
+        <tr>
+          <th>Codigo</th>
+          <th>Nombre</th>
+          <th>Stock</th>
+          <th>Precio</th>
+        </tr>
+        <tr v-for="game in juegos"  :style="{color:game.color}" :key='game.codigo'>
+          <td>{{game.codigo}}</td>
+          <td>{{game.nombre}}</td>          
+          <td>{{game.stock}}</td>
+          <td>{{Number(game.precio).toLocaleString('es-CL')}}</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+//maphelper  
+import { mapState } from "vuex";
+export default {
+  name: "GameList",
+  // props: {},
+  data: function () {
+    return {};
+  },
+  computed: {
+    ...mapState(["juegos"]),
+  },
+  //methods: {}
+  // watch: {},
+  // components: {},
+  // mixins: [],
+  // filters: {},
+  // -- Lifecycle Methods
+  // -- End Lifecycle Methods
+};
+</script>
+
+<style scoped>
+
+.subtitle{
+    font-size: 20px;
+}
+
+table, th, td {
+  border:1px solid black
+}
+
+table{
+    width: 700px;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+</style>
---
+++ src/views/Games.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1 class="title">Tienda 32 Bits</h1>
+        <game-list></game-list>
+    </div>    
+</template>
+
+<script>
+import GameList from '@/components/GameList.vue'
+export default {
+    name: 'games-component',
+    // props: {},
+    data: function(){
+        return {}
+    },
+    // computed: {},
+    //methods: {},
+    // watch: {},
+    components: {
+        'game-list':GameList
+    },
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+    .title{
+        font-size:40px,
+    }
+</style>
